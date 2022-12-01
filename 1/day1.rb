78b7a07b8dcab284c998f62d1f77eac811a963b1
Patricia Kihara <patricia.kihara@outlook.com>
2022-12-01 17:43:24 -0300

    refactor: nicer sorting

--- 1/day1.rb
+++ 1/day1.rb
@@ -1,30 +1,16 @@
-input = File.read('input.txt').lines.map { |line| line.chomp.to_i }
-example = File.read('example.txt').lines.map { |line| line.chomp.to_i }
+input = File.read('input.txt')
+example = File.read('example.txt')
 
-def sort_fruits(input)
-  fruits = []
-  index = 0
-
-  input.each do |num|
-    if num.zero?
-      index += 1
-    elsif fruits[index]
-      fruits[index] << num
-    else
-      fruits << [num]
-    end
-  end
-  fruits
+def sort_by_elf(input)
+  input.split("\n\n").map { |group| group.split("\n").map(&:to_i) }
 end
 
 def solve1(input)
-  fruits_by_elf = sort_fruits(input)
-  fruits_by_elf.map(&:sum).max
+  sort_by_elf(input).map(&:sum).max
 end
 
 def solve2(input)
-  fruits_by_elf = sort_fruits(input)
-  fruits_by_elf.map(&:sum).max(3).sum
+  sort_by_elf(input).map(&:sum).max(3).sum
 end
 
 p solve2(input)
