2a81dafcff1e6054109cce8ae588545b9f2e2adf
Patricia Kihara <patricia.kihara@outlook.com>
2022-12-06 10:07:40 -0300

    style: improvements

--- 6/lib/day6.rb
+++ 6/lib/day6.rb
@@ -7,7 +7,7 @@ def solve1(input)
   end_index = 3
   until input[start_index..end_index].chars.uniq.length == 4
     start_index += 1
-    end_index +=1
+    end_index += 1
   end
   end_index + 1
 end
@@ -19,11 +19,10 @@ def solve2(input)
   end_index = 13
   until input[start_index..end_index].chars.uniq.length == 14
     start_index += 1
-    end_index +=1
+    end_index += 1
   end
   end_index + 1
 end
-
 
 # p solve1(input)
 p solve2(input)
